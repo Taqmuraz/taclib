a5bfbce67dc2a4e935c1ed47651a3583875df9bc
lulook <lulookgamedev@gmail.com>
2025-02-15 20:35:32 +0300

    comp and mcomp

--- funcs.lisp
+++ funcs.lisp
@@ -7,3 +7,25 @@
 (defun part (func &rest pre-args)
   (lambda (&rest args) (apply func (append pre-args args)))
 )
+
+(defun comp (func &rest funcs)
+  (lambda (&rest args)
+    (loop for f in funcs
+      with r = (apply func args)
+      do (setf r (funcall f r))
+      finally (return r)
+    )
+  )
+)
+
+(defmacro mcomp (func &rest funcs)
+  (lets (args (gensym))
+    `(lambda (&rest ,args) ,
+      (loop for f in funcs
+        with r = `(apply ,func ,args)
+        do (setf r `(funcall ,f ,r))
+        finally (return r)
+      )
+    )
+  )
+)
--- package.lisp
+++ package.lisp
@@ -38,5 +38,7 @@
     #:l+ #:l- #:l* #:l/
     #:part
     #:mpart
+    #:comp
+    #:mcomp
   )
 )
